5ba66b9532b406a085af82b8b14afac38f702813
ehlovader <joe.at.trends+github@gmail.com>
2014-03-13 19:42:40 -0400

    Update action with simple luminous resource hooks.

--- action.php
+++ action.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * Example Action Plugin:   Example Component.
+ * Luminous action plugin
  *
- * @author     Samuele Tognini <samuele@cli.di.unipi.it>
+ * @author Joseph Richardson <j4h.r8n@gmail.com
+ * >
  */
 
 if(!defined('DOKU_INC')) die();
 
 
-class action_plugin_example extends DokuWiki_Action_Plugin {
-
+class action_plugin_luminous extends DokuWiki_Action_Plugin {
+   
     /**
      * Register its handlers with the DokuWiki's event controller
      */
     public function register(Doku_Event_Handler $controller) {
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this,
                                    '_hookjs');
+
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this,
+                                   '_hookcss');
     }
 
     /**
@@ -23,11 +27,43 @@ class action_plugin_example extends DokuWiki_Action_Plugin {
      *
      * @author Samuele Tognini <samuele@cli.di.unipi.it>
      */
+    /**
+     * Hook js Script into page headers
+     * 
+     * @param  [type] $event [description]
+     * @param  [type] $param [description]
+     * @return [type]        [description]
+     */
     public function _hookjs(&$event, $param) {
-        $event->data['script'][] = array(
-                            'type'    => 'text/javascript',
-                            'charset' => 'utf-8',
-                            '_data'   => '',
-                            'src'     => DOKU_PLUGIN.'example/example.js');
+
+        $event->data["script"][] = array ("type" => "text/javascript",
+            "charset" => "utf-8",
+            "_data" => "",
+            "src" => DOKU_PLUGIN. "/vendor/luminous-v0.7.0/client/luminous.js"
+            );
+
     }
+
+    /**
+     * Hook CSS styles into page headers
+     * 
+     * @param  [type] $event [description]
+     * @param  [type] $param [description]
+     * @return [type]        [description]
+     */
+    public function _hookcss(&$event, $param){
+
+      $event->data["link"][] = array (
+        "type" => "text/css",
+        "rel" => "stylesheet", 
+        "href" => DOKU_PLUGIN. "/vendor/luminous-v0.7.0/style/luminous.css",
+      );
+      $event->data["link"][] = array (
+        "type" => "text/css",
+        "rel" => "stylesheet", 
+        "href" => DOKU_PLUGIN. "/vendor/luminous-v0.7.0/style/luminous_light.css",
+      );
+
+    }
+
 }
